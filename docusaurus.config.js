93c8e720bf222b80ae17873fa73f5a3a65418946
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-03-28 21:43:25 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'Bitlayer',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/bitlayer.jpeg',
         },
         items: [
           {
