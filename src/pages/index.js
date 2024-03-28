43b89ee9788ccf9be11c8351b75b125360b3de59
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-03-29 00:24:07 +0800

    sbui

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/aboutbitlayer/introduction">
-            Docusaurus Tutorial - 5min ⏱️
+            Start Building
           </Link>
         </div>
       </div>
