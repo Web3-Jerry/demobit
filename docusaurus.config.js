8e76eb11abdeaae5ac8bd694758288cc216db247
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-03-28 20:48:37 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Bitlayer',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/demobit/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Bitlayer', // Usually your GitHub org/user name.
+  projectName: 'Document', // Usually your repo name.
 
   presets: [
     [
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Bitlayer',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
