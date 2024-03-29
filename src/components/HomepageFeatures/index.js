7d399873fe161549ab89aad1e93a252c040c38c7
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-03-29 12:16:51 +0800

    1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,71 +6,45 @@ const FeatureList = [
   {
     title: 'Learn more about Bitlayer',
     Svg: require('@site/static/img/Architecture.svg').default,
-    link: '/demobitlayer/docs/AboutBitlayer/intro/',
     description: (
       <>
-        Bitlayer pioneers with its BitVM approach to solve the trade-off between security and Turing completeness, enabling a robust Bitcoin layer 2 solution.{' '}
+        Bitlayer pioneers with its BitVM approach to solve the trade-off between security and Turing completeness, enabling a robust Bitcoin layer 2 solution.
       </>
     ),
   },
   {
     title: 'Faucet',
     Svg: require('@site/static/img/faucet.svg').default,
-    link: 'https://www.bitlayer.org/faucet',
     description: (
       <>
-        Obtain your Bitlayer Testnet tokens every 24 hours for development here.{' '}
+        Obtain your Bitlayer Testnet tokens every 24 hours for development here.
       </>
     ),
   },
   {
     title: 'Testnet Scan',
     Svg: require('@site/static/img/scan.svg').default,
-    link: 'https://testnet-scan.bitlayer.org/en-us/home',
     description: (
       <>
-        A essential tool for exploring and analyzing blockchain data on the testnet. You can dive deep into the testnet's transactions, blocks, and addresses. {' '}
+        A essential tool for exploring and analyzing blockchain data on the testnet. You can dive deep into the testnet's transactions, blocks, and addresses.
       </>
     ),
   },
 ];
 
-function Feature({ Svg, title, description, link }) {
-  if (title === 'title') {
-    return (
-      <div className="col col--12 text--center">
-        <Heading as="h2">
-          <a href={link} className="custom-link">
-            {description}
-          </a>
-        </Heading>
-      </div>
-    );
-  }
-
+function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <a href={link} className="custom-link">
-          <Svg className={styles.featureSvg} role="img" />
-        </a>
+        <Svg className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">
-          <a href={link} className="custom-link">
-            {title}
-          </a>
-        </Heading>
-        <p>
-          <a href={link} className="custom-link">
-            {description}
-          </a>
-        </p>
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
     </div>
   );
 }
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
