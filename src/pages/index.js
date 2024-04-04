f497bf1cb560e213f04eddee4776fd7b282a5bd4
Web3-Jerry <151769490+Web3-Jerry@users.noreply.github.com>
2024-04-04 13:02:55 +0800

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/aboutbitlayer/introduction"
+            to="/docs/overview/overview"
           >
             Start Building
           </Link>
